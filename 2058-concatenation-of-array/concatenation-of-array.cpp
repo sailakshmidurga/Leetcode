beef4351ff30c2672bb61a64a82bc3ee1e0a35d8
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2025-07-22 22:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (17.65%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        for(int i =0;i<2;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                ans.push_back(nums[j]);
+            }
+        }
+        return ans;
+    }
+};
