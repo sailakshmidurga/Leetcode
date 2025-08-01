8105a92ead0a719fd93720a158d9250edc237ef3
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2025-08-01 22:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (31.83%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int res = 0;
+        int i = s.length()-1;
+       while(i>=0 && s[i]==' ')
+       {
+        i--;
+       }
+       while(i>=0 && s[i]!=' ')
+       {
+        res++;
+        i--;
+       }
+       return res;
+    }
+};
