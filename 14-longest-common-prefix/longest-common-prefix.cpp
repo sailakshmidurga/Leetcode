72ee32984c9934deb4d23977c0ff6c7aa027790b
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2025-08-12 22:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (48.80%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       if (strs.empty()) return "";
+
+    std::string prefix = strs[0];
+
+    for (size_t i = 1; i < strs.size(); ++i) {
+        size_t j = 0;
+        while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+            ++j;
+        }
+        prefix = prefix.substr(0, j);
+        if (prefix.empty()) break;
+    }
+
+    return prefix;
+
+    }
+};
