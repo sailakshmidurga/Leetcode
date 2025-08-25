ed8748672f0e4fa04fbf2d0d5ffb88e95a62f317
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2025-08-25 21:06:44 +0530

    Time: 4 ms (53.59%) | Memory: 117.8 MB (97.20%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+    ListNode* reverse(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr != nullptr) {
+        ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+public:
+    bool isPalindrome(ListNode* head) {
+     if (head == nullptr || head->next == nullptr) return true;
+
+    // Step 1: Find the middle using slow and fast pointers
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Step 2: Reverse the second half
+    ListNode* secondHalf = reverse(slow);
+    ListNode* firstHalf = head;
+
+    // Step 3: Compare both halves
+    while (secondHalf != nullptr) {
+        if (firstHalf->val != secondHalf->val)
+            return false;
+        firstHalf = firstHalf->next;
+        secondHalf = secondHalf->next;
+    }
+
+    return true;
+    }
+};
