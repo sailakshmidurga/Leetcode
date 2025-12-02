7dbbdb48dd79cf8b305473244c05b0314784117d
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2025-12-02 19:43:15 +0530

    Time: 3 ms (61.49%) | Memory: 127.3 MB (28.66%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,43 +9,28 @@
  * };
  */
 class Solution {
-    public:
-    ListNode* reverse(ListNode* head) {
-    ListNode* prev = nullptr;
-    ListNode* curr = head;
-    while (curr != nullptr) {
-        ListNode* next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
-
 public:
     bool isPalindrome(ListNode* head) {
-     if (head == nullptr || head->next == nullptr) return true;
-
-    // Step 1: Find the middle using slow and fast pointers
-    ListNode* slow = head;
-    ListNode* fast = head;
-    while (fast != nullptr && fast->next != nullptr) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    // Step 2: Reverse the second half
-    ListNode* secondHalf = reverse(slow);
-    ListNode* firstHalf = head;
-
-    // Step 3: Compare both halves
-    while (secondHalf != nullptr) {
-        if (firstHalf->val != secondHalf->val)
-            return false;
-        firstHalf = firstHalf->next;
-        secondHalf = secondHalf->next;
-    }
-
-    return true;
+        stack<int>st;
+        ListNode* temp = head;
+        while(temp!=nullptr)
+        {
+            st.push(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while(temp!=nullptr)
+        {
+            if(temp->val != st.top())
+            {
+                return false;
+            }
+            else
+            {
+                st.pop();
+                temp = temp->next;
+            }
+        }
+        return true;
     }
 };
