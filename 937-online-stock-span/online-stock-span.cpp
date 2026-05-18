8966d387f923ec8bd5c12ea9588f2bd85a3b361c
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-05-18 15:15:16 +0530

    Time: 527 ms (5.06%) | Memory: 93.1 MB (10.51%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.cpp
@@ -0,0 +1,24 @@
+class StockSpanner {
+    vector<int>arr;
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        arr.push_back(price);
+        int cnt = 1;
+        for(int i = arr.size()-2;i>=0;i--)
+        {
+            if(arr[i] <= price) cnt++;
+            else break;
+        }
+        return cnt;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
