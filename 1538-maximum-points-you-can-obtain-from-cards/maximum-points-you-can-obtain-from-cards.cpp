16f7f2050a0c764e9ab1d0895c37ea37fe963318
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-05-24 21:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (31.96%) - LeetSync

---
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+         int n = cardPoints.size();
+        int ls = 0;
+        int rs = 0;
+        for(int i = 0;i<k;i++)
+        {
+            ls += cardPoints[i];
+        }
+        int maxLen = ls;
+        int rightIndex = n-1;
+        for(int i = k-1;i>=0;i--)
+        {
+            ls -= cardPoints[i];
+            rs += cardPoints[rightIndex];
+            rightIndex--;
+            maxLen = max(maxLen,ls+rs);
+        }
+        return maxLen;
+    }
+};
