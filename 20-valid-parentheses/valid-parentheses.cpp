a2c55dc541da2790cb050d5cf0e7a3c6e1ed67fc
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-06-07 12:44:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (5.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(char c:s)
-        {
-            if(c == '(' || c == '{' || c== '[')
+       stack<int>st;
+       for(char c:s)
+       {
+            if(c == '(' || c == '[' || c == '{')
             {
                 st.push(c);
             }
             else
             {
                 if(st.empty()) return false;
-                if(st.top() == '(' && c == ')' || st.top() == '{' && c == '}' || st.top() == '[' && c == ']')
+                if(st.top() == '(' && c == ')' || st.top() == '[' && c == ']' || st.top() == '{' && c == '}' )
                 {
                     st.pop();
                 }
-                else
-                {
+                else{
                     return false;
                 }
             }
-        }
-        return st.empty();
+       }
+       return st.empty();
     }
 };
