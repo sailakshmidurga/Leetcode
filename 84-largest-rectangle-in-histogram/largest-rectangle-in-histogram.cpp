1ba88bc58d13aac9008b453c21d51da56060bb7e
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-06-08 12:29:58 +0530

    Time: 27 ms (37.67%) | Memory: 87.8 MB (13.01%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,48 +1,50 @@
-class Solution {
+class Solution
+{
     public:
-    vector<int>findNse(vector<int>& nums)
+    vector<int>findNse(vector<int> &heights)
     {
-        int n = nums.size();
-        stack<int>st;
+        int n = heights.size();
         vector<int>nse(n);
+        stack<int>st;
         for(int i = n-1;i>=0;i--)
         {
-             while(!st.empty() && nums[st.top()] >= nums[i])
-             {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+            {
                 st.pop();
-             }
-             nse[i] = st.empty()?n:st.top();
-             st.push(i);
+            }
+            nse[i] = st.empty()?n:st.top();
+            st.push(i);
         }
         return nse;
     }
+
     public:
-    vector<int>findPse(vector<int>& nums)
+    vector<int>findPse(vector<int> &heights)
     {
-        int n = nums.size();
-        stack<int>st;
+        int n = heights.size();
         vector<int>pse(n);
+        stack<int>st;
         for(int i = 0;i<n;i++)
         {
-             while(!st.empty() && nums[st.top()] > nums[i])
-             {
+            while(!st.empty() && heights[st.top()] > heights[i])
+            {
                 st.pop();
-             }
-             pse[i] = st.empty()?-1:st.top();
-             st.push(i);
+            }
+            pse[i] = st.empty()?-1:st.top();
+            st.push(i);
         }
         return pse;
     }
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int>nse = findNse(heights);
-        vector<int>pse = findPse(heights);
-        int maxi = 0;
-        for(int i = 0;i<n;i++)
-        {
-            maxi = max(maxi,heights[i] * (nse[i]-pse[i]-1));
-        }
-        return maxi;
+    int largestRectangleArea(vector<int> &heights) {
+      int n = heights.size();
+      vector<int>pse = findPse(heights);
+      vector<int>nse = findNse(heights);
+      int maxi = 0;
+      for(int i = 0;i<n;i++)
+      {
+        maxi = max(maxi,heights[i] * (nse[i]-pse[i]-1));
+      }
+      return maxi;
     }
 };
