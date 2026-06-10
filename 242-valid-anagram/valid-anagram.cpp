3c07d4ca71d5f56c5dc9425ae8bd2ace4085c5d5
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-06-10 22:29:17 +0530

    Time: 4 ms (34.32%) | Memory: 9.9 MB (10.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        int n1 = s.size();
+        int n2 = t.size();
         bool res = true;
-        map<int,int>mpp1;
-        map<int,int>mpp2;
+        unordered_map<char,int>mpp1;
+        unordered_map<char,int>mpp2;
+
         for(char c:s)
         {
             mpp1[c]++;
         }
-        for(char c:t)
+        for(char c: t)
         {
             mpp2[c]++;
         }
         for(char c:s)
         {
-            if(t.find(c)==string::npos)
+            if(t.find(c) == string::npos)
             {
                 res = false;
+                break;
             }
-            if(mpp1[c]!=mpp2[c]){
+            if(mpp1[c]!=mpp2[c])
+            {
                 res = false;
                 break;
-                }
+            }
         }
         for(char c:t)
         {
-            if(s.find(c)==string::npos)
+            if(s.find(c) == string::npos)
             {
                 res = false;
+                break;
             }
-            if(mpp1[c]!=mpp2[c]){
+            if(mpp1[c]!=mpp2[c])
+            {
                 res = false;
                 break;
-                }
+            }
         }
         return res;
     }
