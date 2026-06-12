da8ee2c353b11ae6d85f5980cd24b54ce7d8b57f
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-06-12 12:11:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.33%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,23 @@
 class Solution {
+    public:
+    double solve(double x,long long n,double ans)
+    {
+        if(n == 0) return ans;
+
+        if(n%2==0)
+        {
+            return solve(x*x,n/2,ans);
+        }
+            return solve(x,n-1,x*ans);
+    }
 public:
     double myPow(double x, int n) {
-        double ans = 1.0;
         long long m = n;
-        if(n<0) m=-m;
-        while(m!=0)
+        if(m<0)
         {
-            if(m%2==0)
-            {
-                x = x*x;
-                m = m/2;
-            }
-            else
-            {
-                ans *= x;
-                m = m-1;
-            }
+            m = -m;
         }
-        return (n<0)?1.0/ans:ans;
+        double ans = solve(x,m,1.0);
+        return (n<0)?(1.0/ans):ans;
     }
 };
