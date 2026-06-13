b8af5358e2f5512f73a6f3e4fb9f244ba19a9634
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-06-13 19:31:18 +0530

    Time: 19 ms (5.44%) | Memory: 43.6 MB (77.60%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         string res = "";
-        unordered_map<char,int>mpp;
-        for(char ch = 'a';ch<= 'z';ch++)
-        {
-            mpp[ch] = weights[ch - 'a'];
-        }
         for(int i = 0;i<words.size();i++)
         {
             int num = 0;
             for(int j = 0;j<words[i].size();j++)
             {
-                num += mpp[words[i][j]];
+                num += weights[words[i][j] - 'a'];
             }
             int modulo = num % 26;
             cout<<modulo;
