8e54c16d6f471dc9db70860f104684a8910274c6
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-07-23 16:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.47%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int myAtoi(string s) {
-         int n = s.size();
-        int sign = 1;
-        int i = 0;
-        while(i<n && s[i] == ' ')
+       int i = 0;
+       int n = s.size();
+       long long res = 0;
+       while(i<n && s[i] == ' ') 
+       {
+         i++;
+       }
+       int sign = 1;
+       if(i<n && s[i] == '+' || s[i] == '-')
+       {
+        if(s[i] == '-')
         {
-            i++;
+            sign = -1;
         }
-        if(i<n && (s[i] == '+' || s[i] == '-'))
-        {
-            sign = (s[i] == '-')?-1:1;
-            i++;
+        i++;
+       }
+       while(i<n && isdigit(s[i]))
+       {
+        res = res * 10 + (s[i] - '0');
+        i++;
+        if(sign*res <= INT_MIN) return INT_MIN;
+        if(sign*res >= INT_MAX) return INT_MAX;  
         }
-        long long num = 0;
-        while(i<n && isdigit(s[i]))
-        {
-            int digit = s[i] - '0';
-            num = num * 10 + digit;
-            if(num*sign >= INT_MAX) return INT_MAX;
-            else if(num*sign <= INT_MIN) return INT_MIN;
-            i++;
-        }
-        return (int)sign * num;
+        return res*sign;
     }
 };
