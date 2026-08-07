091829b2279887f9357cc729010eb553f67e3eb0
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-08-07 15:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (89.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 || n== 1) return n; 
-        return fib(n-1)+fib(n-2);
+        if(n == 0 || n== 1) return n;
+        int prev1 = 0;
+        int prev2 = 1;
+        for(int i = 2;i<=n;i++)
+        {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
     }
 };
