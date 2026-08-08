453387b785cd6a9ffbbba3fe14ddebaed31ac21d
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-08-08 13:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (7.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-         int mini = INT_MAX;
-        int maxProfit = 0;
+        int mini = INT_MAX;
+        int profit = 0;
         for(int i = 0;i<n;i++)
         {
             mini = min(mini,prices[i]);
-            maxProfit = max(maxProfit,prices[i] - mini);
+            profit = max(profit,prices[i]-mini);
         }
-        return maxProfit;
+        return profit;
     }
 };
