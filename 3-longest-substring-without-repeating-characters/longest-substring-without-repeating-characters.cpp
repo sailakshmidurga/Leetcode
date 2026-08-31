d1b62cf46c47beee1587fd456e1e2bc4d49bf276
Sai Lakshmi Durga Koneti <konetisailakshmidurga@gmail.com>
2026-08-31 10:28:58 +0530

    Time: 46 ms (38.07%) | Memory: 19.4 MB (24.06%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int r = 0;
         int l = 0;
-        int maxLen = 0;
-        unordered_map<int,int>mpp;
+        int r = 0;
+        int maxlen = 0;
+        unordered_map<char,int>mpp;
         while(r<n)
         {
             if(mpp.find(s[r]) != mpp.end())
             {
-                l = max(l, mpp[s[r]] + 1);
+                l = max(l,mpp[s[r]]+1);
             }
-             maxLen = max(maxLen,r-l+1);
-             mpp[s[r]] = r;
-             r++;
+            maxlen = max(maxlen,r-l+1);
+            mpp[s[r]] = r;
+            r++;
+
         }
-        return maxLen;
+        return maxlen;
     }
 };
